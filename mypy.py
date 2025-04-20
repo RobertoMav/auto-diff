75316fa343699259e793a7f87cc44a22d9e134bb
Roberto Martins <robertomartins@Robertos-MacBook-Pro.local>
2025-04-19 23:55:06 -0300

    Adding uv + testing new linters

---
+++ mypy.py
@@ -0,0 +1,23 @@
+var: int = 1
+
+from typing import Any, Dict, List, Tuple
+
+var = "hello"
+
+
+class MyClass:
+    def __init__(self, var: int, var2: int, var3: int):
+        self.var = var
+        self.var2 = var2
+        self.var3 = var3
+
+
+def my_function(var: int, var2: int, var3: Any) -> int:
+    return var + var2 + var3
+
+
+List[int] = [1, 2, 3]
+
+Dict[str, int] = {"a": 1, "b": 2, "c": 3}
+
+Tuple[int, str, bool] = (1, "hello", True)
---
+++ python.py
@@ -0,0 +1,48 @@
+import torch
+
+
+def add_numbers(a: int, b: int) -> int:
+    """_summary_
+
+    Args:
+        a (int): _description_
+        b (int): _description_
+
+    Returns:
+        int: _description_
+    """
+    return a + b
+
+
+print(add_numbers(1, 2))
+
+
+
+x: torch.Tensor = torch.Tensor(1.0)
+y: torch.Tensor = torch.Tensor(2.0)
+
+z: torch.Tensor = x + y
+print(z)
+print(type(z))
+
+print(z.item())
+print(type(z.item()))
+
+
+def function_nice(
+    a: int,
+    b: int,
+    c: int,
+) -> int:
+    """
+    This function is a nice function.
+    """
+    c = a + b + c
+    return c
+
+
+VALUE = "hi"
+
+VALUE.strip("a").strip("b").strip("c").strip("a").strip("b").strip("c").strip("a").strip(
+    "a"
+).strip("a").strip("a").strip("a").strip("a").strip("a").strip("a").strip("a").strip("a")
