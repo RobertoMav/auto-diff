96fc67c42b864430cbb10e696ffb19dd68c9c357
RobertoMav <robertomartinsv11@gmail.com>
2025-05-02 23:28:23 -0300

    Adding simple typing learning

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
 echo "Yeah"
+
+python3 test-typing.py
---
+++ test-typing.py
@@ -0,0 +1,32 @@
+from typing import TypedDict, TypeVar
+
+
+class Person(TypedDict):
+    name: str
+    age: int
+
+
+print(Person(name="John", age=30))
+
+
+J = TypeVar("J")
+
+def foo(x: J) -> J:
+    return x
+
+print(foo(1))
+print(foo("hello"))
+
+
+def foo2[T](x: T) -> T:
+    """
+    This function takes an argument of any type 'T'
+    and returns a value of the same type 'T'.
+    The '[T]' declares the type variable.
+    """
+    return x
+
+print(foo2(1))
+print(foo2("hello"))
+
+
